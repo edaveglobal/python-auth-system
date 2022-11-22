efe3c4f1724a845e88d9182f6eac29fd9939da67
Arafat Olayiwola <harof.dev@gmail.com>
2022-11-22 15:27:42 +0100

    add some testcases login and register

---
+++ auth_system/accounts/TestsCases/configs.py
@@ -0,0 +1,17 @@
+
+class Configs():
+    register_url = "/api/v1/users/account"
+    login_url = "/api/token/"
+    user_details_url = "/api/v1/users/accounts/"
+
+    user_register_data = {
+        "email": "test@example.com", 
+        "username": "test_user",
+        "first_name": "test3",
+        "last_name": "test3",
+        "password": "verysecret"
+    }
+    user_login_data = {
+        "username": "test_user",
+        "password": "verysecret"
+    }
