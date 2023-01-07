ff761327b0a33770b10d98d534b48fed07dbc2d0
Arafat Olayiwola <harof.dev@gmail.com>
2023-01-07 17:08:44 +0100

    fix settings HOST

--- auth_system/auth_system/settings.py
+++ auth_system/auth_system/settings.py
@@ -24,9 +24,9 @@ if not DEBUG:
     PROTOCOL = "https"
     DOMAIN = "https://gathpay-auth-qb9l6.ondigitalocean.app/"
 
-CSRF_TRUSTED_ORIGINS = ['https://gathpay-auth-qb9l6.ondigitalocean.app/']
+CSRF_TRUSTED_ORIGINS = ['https://gathpay-auth-qb9l6.ondigitalocean.app']
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://gathpay-auth-qb9l6.ondigitalocean.app/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'gathpay-auth-qb9l6.ondigitalocean.app']
 
 
 # Application definition
