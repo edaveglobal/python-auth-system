9f1a94fae6a4e6006f8554d4891c503ecb15f4ab
Arafat Olayiwola <harof.dev@gmail.com>
2023-01-08 20:12:55 +0100

    fix entrypoint script

--- auth_system/entrypoint.sh
+++ auth_system/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 APP_PORT=${PORT:-8000}
 
-/opt/venv/bin/python manage.py collectstatic --noinput
+# /opt/venv/bin/python manage.py collectstatic --noinput
 /opt/venv/bin/python manage.py migrate --noinput
 /opt/venv/bin/gunicorn  --bind "0.0.0.0:${APP_PORT}" \
          --workers=3 --worker-class=gevent \
